16279032d6c8c21d415efff487afde33943a638a
Augustine Ezinwa <augustineezinwa@gmail.com>
2024-11-23 10:41:05 +0100

    fix workflow

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  //I want to host this as a static site, so update with the required config
-  output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "",
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
